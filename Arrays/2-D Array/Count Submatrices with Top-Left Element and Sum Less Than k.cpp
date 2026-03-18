903ca7239ad448405a8421128b10e615e29156cd
MAZHAR <iiestshibpur605@gmail.com>
2026-03-18 07:08:04 +0530

    Added YouTube Link

--- Arrays/2-D Array/Count Submatrices with Top-Left Element and Sum Less Than k.cpp
+++ Arrays/2-D Array/Count Submatrices with Top-Left Element and Sum Less Than k.cpp
@@ -1,6 +1,6 @@
 /*     Scroll below to see JAVA code also   */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=sWNfDG1kyrA
     Company Tags                : will update later
     Leetcode Link               : https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k
 */
